e78f643289ac3703a53fc665055905a526198991
S <s>
2017-06-08 13:21:55 +0100

    {"types":"C","runs":53}

--- Breakout.java
+++ Breakout.java
@@ -93,9 +93,9 @@ public class Breakout extends GraphicsProgram {
 		    colStart = j * (colPosition);
 		    
 		GRect myBricks = new GRect (rowStart, colStart, BRICK_WIDTH, BRICK_HEIGHT);
-		myBricks.setFilled(true);
+		//myBricks.setFilled(true);
 		if (colourInc>20){
-			myBricks.setColor(Color.RED);
+			myBricks.setFillColor(Color.RED);
 		} else if (colourInc>40){
 			myBricks.setColor(Color.BLUE);	
 		}
