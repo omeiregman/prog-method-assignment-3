d77f7ab919e950794390c365d70ffa414c421f15
S <s>
2017-06-09 13:27:57 +0100

    {"types":"C","runs":111}

--- Breakout.java
+++ Breakout.java
@@ -82,8 +82,8 @@ public class Breakout extends GraphicsProgram {
 		
 		println(xCenter + "  " + yCenter + BRICK_WIDTH);
 		
-		double rowStart = 1;
-		double colStart = BRICK_Y_OFFSET;
+		double rowStart = BRICK_Y_OFFSET;
+		double colStart = xCenter - (BRICK_WIDTH * NBRICKS_PER_ROW)/2;
 		
 		double rowPosition = BRICK_WIDTH + BRICK_SEP;
 		double colPosition = BRICK_HEIGHT + BRICK_SEP;
