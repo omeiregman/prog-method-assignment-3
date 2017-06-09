07011ffaf4a6c2ff11b4547095d0f56be1977a06
S <s>
2017-06-09 13:55:47 +0100

    {"types":"C","runs":136}

--- Breakout.java
+++ Breakout.java
@@ -92,7 +92,7 @@ public class Breakout extends GraphicsProgram {
 			for (int j=1; j<NBRICK_ROWS+1; j++) {
 				
 			rowStart = i * (rowPosition);
-		    colStart = j * (colPosition);
+		    colStart = BRICK_Y_OFFSET +(j * colPosition);
 		    
 		GRect myBricks = new GRect (rowStart, colStart, BRICK_WIDTH, BRICK_HEIGHT);
 		add(myBricks);
